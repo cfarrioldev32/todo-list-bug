d298bf9bda0d977d1ad01ca9def2ae2bebc1247e
cfarrioldev32 <cristianfarriol28@gmail.com>
2025-11-25 18:43:16 +0100

    feat(auth): add CurrentUser decorator and JWT payload extraction

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,22 +1,24 @@
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { TasksService } from './tasks.service';
+import { CurrentUser } from 'src/auth/decorators/current-user.decorator';
+import { JwtPayload } from 'src/auth/interfaces/jwt-payload.interface';
 
 @Controller('tasks')
 export class TasksController {
     constructor(private readonly tasksService: TasksService) {}
 
     @Get('')
-    async listTasks() {
-        return this.tasksService.listTasks();
+    async listTasks(@CurrentUser() currentUser: JwtPayload) {
+        return this.tasksService.listTasks(currentUser.id);
     }
 
     @Get('/:id')
-    async getTask(@Param('id') id: string) {
-        return this.tasksService.getTask(id);
+    async getTask(@Param('id') id: string, @CurrentUser() currentUser: JwtPayload) {
+        return this.tasksService.getTask(id, currentUser.id);
     }
 
     @Post('/edit')
-    async editTask(@Body() body) {
-        return this.tasksService.editTask(body);
+    async editTask(@Body() body, @CurrentUser() currentUser: JwtPayload) {
+        return this.tasksService.editTask(body, currentUser.id);
     }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Task } from '../entities/task.entity';
 import { Repository } from 'typeorm';
@@ -10,25 +10,33 @@ export class TasksService {
         private readonly tasksRepository: Repository<Task>,
     ) {}
 
-    async listTasks() {
-        const tasks = await this.tasksRepository.find();
-
+    async listTasks(userId: string) {
+        const tasks = await this.tasksRepository.find({
+            where: {owner: {id: userId}}
+        });
+        //TODO: Implement logs
         return tasks;
     }
 
-    async getTask(id: string) {
-        const task = await this.tasksRepository
-            .createQueryBuilder('task')
-            .where(`task.id = "${id}"`)
-            .getOne();
-
+    async getTask(taskId: string, userId: string) {
+        const task = await this.tasksRepository.findOne({
+            where: { id: taskId, owner: {id: userId} },
+            relations: ['owner'],
+            select: {
+                owner: {
+                    id: true
+                }
+            }
+        },
+        );
+        //TODO: Implement logs
         return task;
     }
 
-    async editTask(body: any) {
+    async editTask(body: any, userId: string) {
         await this.tasksRepository.update(body.id, body);
 
-        const editedTask = await this.getTask(body.id);
+        const editedTask = await this.getTask(body.id, userId);
 
         return editedTask;
     }
